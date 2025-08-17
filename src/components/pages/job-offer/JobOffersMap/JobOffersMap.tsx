9c2bca61a116228c4a86088a2892de0955468af8
Tomasz Kierzenkowski <zivavu@gmail.com>
2025-08-17 19:31:21 +0200

    :lipstick: Light mode map styling

--- src/components/pages/job-offer/JobOffersMap/JobOffersMap.tsx
+++ src/components/pages/job-offer/JobOffersMap/JobOffersMap.tsx
@@ -2,38 +2,45 @@
 
 import * as maptilersdk from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
-import { Box } from '@mui/material';
+import { Box, useColorScheme } from '@mui/material';
 import { useEffect, useRef } from 'react';
 import { TOP_OFFSET } from '../../home/const';
 import { JobOfferComponentProps } from '../types';
-
-const CUSTOM_STYLE_URL =
-	'https://api.maptiler.com/maps/ce102dd9-4063-4186-a993-ff95a539fca7/style.json?key=z7iGzSLh53F8TdrmZbkk';
 
 const MAPTILER_API_KEY = process.env.NEXT_PUBLIC_MAPTILER_API_KEY;
 export function JobOffersMap({ offer }: JobOfferComponentProps) {
 	const mapContainer = useRef<HTMLDivElement>(null);
 	const map = useRef<maptilersdk.Map>(null);
+	const { mode } = useColorScheme();
 
-	if (!offer) return null;
-	const softwareHouseLocation = offer?.location;
+	const styleAdress =
+		mode === 'dark'
+			? 'ce102dd9-4063-4186-a993-ff95a539fca7'
+			: '0197cb04-ec1b-7452-bc0d-39a02f4ba039';
+	const CUSTOM_STYLE_URL = `https://api.maptiler.com/maps/${styleAdress}/style.json?key=z7iGzSLh53F8TdrmZbkk`;
 
-	const zoom = 14;
-	if (!MAPTILER_API_KEY) return <div>Provide a Maptiler API key</div>;
-	maptilersdk.config.apiKey = MAPTILER_API_KEY;
-
-	// /maps/ce102dd9-4063-4186-a993-ff95a539fca7/style.json
 	useEffect(() => {
+		if (!offer?.location) return;
 		if (map.current) return;
 		if (!mapContainer.current) return;
 
 		map.current = new maptilersdk.Map({
 			container: mapContainer.current,
 			style: CUSTOM_STYLE_URL,
-			center: [softwareHouseLocation.lng, softwareHouseLocation.lat],
-			zoom: zoom,
+			center: [offer?.location.lng, offer?.location.lat],
+			zoom: 14,
 		});
-	}, [softwareHouseLocation.lng, softwareHouseLocation.lat, zoom]);
+	}, [offer]);
+
+	useEffect(() => {
+		if (!map.current) return;
+		map.current.setStyle(CUSTOM_STYLE_URL);
+	}, [mode]);
+
+	if (!MAPTILER_API_KEY) return <div>Provide a Maptiler API key</div>;
+	maptilersdk.config.apiKey = MAPTILER_API_KEY;
+
+	if (!offer) return null;
 	return (
 		<Box
 			sx={{
